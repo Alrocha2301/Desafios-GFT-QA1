4ed90f9dbdef50ce28a1db0cd30d0c33f788f68e
Alrocha2301 <alexandre-apr@hotmail.com>
2022-04-01 08:42:53 -0300

    Desafios QA

--- src/GFT/QA/desafios/Exercicio03.java
+++ src/GFT/QA/desafios/Exercicio03.java
@@ -1,18 +1,41 @@
 package GFT.QA.desafios;
 
-Ramsay: "(...) você vence se conseguir adivinhar quem eu sou e por que estou torturando você."
+/*Ramsay: "(...) você vence se conseguir adivinhar quem eu sou e por que estou torturando você."
+        Theon deve pensar rápido e adivinhar quem é seu algoz! Entretanto, Ramsay já decidiu o
+        que ele irá fazer depois que Theon der sua resposta.
 
-        Theon deve pensar rápido e adivinhar quem é seu algoz! Entretanto, Ramsay já decidiu o que ele irá fazer depois que Theon der sua resposta.
+        Theon pode dizer que seu algoz é alguma dentre N pessoas. Considere que as pessoas são
+         numeradas de 1 a N. Se Theon responder que seu algoz é a pessoa i, Ramsay irá atingi-lo Ti vezes.
 
-        Theon pode dizer que seu algoz é alguma dentre N pessoas. Considere que as pessoas são numeradas de 1 a N. Se Theon responder que seu algoz é a pessoa i, Ramsay irá atingi-lo Ti vezes.
-
-        Sua tarefa é ajudar Theon a determinar qual deve ser sua resposta de forma a minimizar o número de vezes que ele será atingido.
+        Sua tarefa é ajudar Theon a determinar qual deve ser sua resposta de forma a minimizar o
+         número de vezes que ele será atingido.
 
         Entrada
-        A primeira linha contém um inteiro N (1 ≤ N ≤ 100). A segunda linha contém N inteiros T1, T2, ..., TN (0 ≤ Ti ≤ 20).
+        A primeira linha contém um inteiro N (1 ≤ N ≤ 100). A segunda linha contém N inteiros T1,
+         T2, ..., TN (0 ≤ Ti ≤ 20).
 
         Saída
-        Imprima uma linha contendo o número da pessoa que Theon deve dizer ser seu algoz. Se existe mais de uma resposta possível, imprima a menor.
+        Imprima uma linha contendo o número da pessoa que Theon deve dizer ser seu algoz. Se existe
+         mais de uma resposta possível, imprima a menor.*/
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Exercicio03 {
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        int T, menor = 0, posMenor = 0;
+        for (int i = 0; i < N; i++ ) {
+            T = leitor.nextInt();
+            if ( i == 0 ) {
+                menor = T;
+                posMenor = i + 1;
+            } else if ( T < menor  ) {
+                menor = T;
+                posMenor = i + 1;
+            }
+        }
+        System.out.println(posMenor);
+    }
 }
